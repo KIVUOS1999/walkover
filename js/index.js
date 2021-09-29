d9de733241f8b4925cd61f3f7af675a65bfa0fbc
Kivuos <sarkar.souvik2016@outlook.com>
2021-09-30 01:04:35 +0530

    made the whole thing flex and fixes some bugs

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-const heading = document.querySelector('.items .heading');
+const heading = document.querySelector('.navbar .heading');
 const writing = document.querySelectorAll('.content-right .text');
+const item = document.querySelectorAll('.navbar .item');
 const image = document.querySelectorAll('.content-right .image');
-const head = document.querySelectorAll('h1');
-const ul = document.querySelectorAll('ul');
+const head = document.querySelectorAll('.content-right h1');
+const ul = document.querySelectorAll('.navbar');
+const topics = document.querySelectorAll('.main .heading');
 const t1 = new TimelineMax()
 t1 .fromTo(
         ul,
@@ -15,6 +17,18 @@ t1 .fromTo(
         0.5,
         {opacity: '0', x: -100},
         {opacity: '1', x: 0, ease: Power2.easeInOut}   
+    )
+    .fromTo(
+        item,
+        0.5,
+        {opacity: '0', x: 100},
+        {opacity: '1', x: 0, ease: Power2.easeInOut}
+    )
+    .fromTo(
+        topics,
+        0.5,
+        {opacity: '0', x: -1000},
+        {opacity: '1', x: 0, ease: Power2.easeInOut} 
     )
     .fromTo(
         head,
